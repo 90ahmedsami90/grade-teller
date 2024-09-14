4e30d50b5ce70862901c739529b83abb54975912
90ahmedsami90 <ahmed45sami54@gmail.com>
2024-09-14 10:08:06 -0700

    Add files via upload

---
+++ percentage/js.js
@@ -0,0 +1,19 @@
+let percentage=prompt("ENTER THE PERCENTAGE");
+
+if(percentage > 100){
+    alert("ERROR")
+} else if(percentage >= 90){
+    alert("A1 GRADE")
+} else if(percentage >= 80){
+    alert("A GRADE")
+} else if(percentage >= 70){
+    alert("B GRADE")
+} else if(percentage >= 60){
+    alert("C GRADE")
+} else if(percentage < 60){
+    alert("FAIL")
+} else if(percentage <= 0){
+    alert("ERROR")
+}  else {
+    alert("ERROR")
+}
